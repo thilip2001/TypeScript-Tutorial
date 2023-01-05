5d66a07514ac915cbcffef254e8b6afcc7a2c8ac
thiliban-pyt <120371707+thiliban-pyt@users.noreply.github.com>
2023-01-05 12:17:19 +0530

    tsc type added

--- sandbox.ts
+++ sandbox.ts
@@ -1,11 +1,18 @@
 // const character = 'Luxe'
 // console.log(character);
+
 // const inputs = document.querySelectorAll("input")
 // console.log(inputs);
+
 // inputs.forEach((input)=> console.log(input))
+
 // let character :string = "Hello" //explicit type add
+
 // let isBoolean = true
+
 // const circ = (diameter : number) =>{
 //     return Math.floor(diameter * Math.PI)
 // }
+
 // console.log(circ(8));
+
